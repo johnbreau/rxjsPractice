8cdfaceaa76322c8abfd9f328a5491b388a3f62d
John Breau <john.breau@vml.com>
2018-10-08 14:14:55 -0500

    added drag and drop observable stream

--- src/app/random-practice/random-practice.component.ts
+++ src/app/random-practice/random-practice.component.ts
@@ -7,23 +7,37 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./random-practice.component.css']
 })
 export class RandomPracticeComponent implements OnInit {
+  public panel: HTMLElement;
 
   constructor() { }
 
   ngOnInit() {
+    // Drap and drop Observable...
+      const panel = document.querySelector('#dragtarget') as HTMLElement;
+      const mouseDown$ = Observable.fromEvent<MouseEvent>(panel, 'mousedown');
+      const mouseUp$ = Observable.fromEvent<MouseEvent>(document, 'mouseup');
+      const mouseMove$ = Observable.fromEvent<MouseEvent>(document, 'mousemove');
+
+      const drag$ = mouseDown$.concatMap(() => mouseMove$.takeUntil(mouseUp$));
+
+      drag$.subscribe(event => {
+        panel.style.left = event.clientX + 'px';
+        panel.style.top = event.clientY + 'px';
+      });
+
     // Subsribe to button clicks, increment and dispaly...
-      //   const button = document.getElementById('button');
-      //   const display = document.getElementById('display');
-      //   const source = Observable.fromEvent(button, 'click');
-      //   let num = 0;
+        const button = document.getElementById('button');
+        const display = document.getElementById('display');
+        const source = Observable.fromEvent(button, 'click');
+        let num = 0;
 
-      //   function incrementNumber() {
-      //     num++;
-      //     display.innerHTML = num.toString();
-      //   }
+        function incrementNumber() {
+          num++;
+          display.innerHTML = num.toString();
+        }
 
-      //   source.subscribe(() =>
-      //     incrementNumber()
-      //   );
+        source.subscribe(() =>
+          incrementNumber()
+        );
   }
 }
